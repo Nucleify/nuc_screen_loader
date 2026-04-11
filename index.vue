55bb82b2e2fe3ae8f99ac75a72bbf79a52dd6513
SzymCode <szymon.radomski@yahoo.com>
2026-04-11 11:23:14 +0200

    chore: apply Biome fixes

--- index.vue
+++ index.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="screen-loader-container">
     <div class="screen-loader-content">
-      <ad-logo-svg
-        class="loader-image"
-        ad-type="main"
-        dimensions="125"
-      />
+      <ad-logo-svg class="loader-image" ad-type="main" dimensions="125" />
       <ad-progress-bar mode="indeterminate" width="100px" height="10px" />
     </div>
   </div>
